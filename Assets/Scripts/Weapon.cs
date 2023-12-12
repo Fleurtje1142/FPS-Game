0f9b69849807a9300efd4bb0602851b0cf0b0705
Fleurtje1142 <f.p.a.vangils@icloud.com>
2023-12-12 13:00:23 +0100

    bullet works better

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -14,7 +14,7 @@ public class Weapon : MonoBehaviour
 
     // Burst
     public int bulletsPerBurst = 3;
-    public int currentBurst;
+    public int burstBulletsLeft;
 
     // Spread (For accuraty of the weapons)
     public float spreadIntensity;
@@ -31,26 +31,101 @@ public class Weapon : MonoBehaviour
         Burst,
         Auto
     }
-   
+
+    public ShootingMode currentShootingMode;
+
+    private void Awake()
+    {
+        readyToShoot = true;
+        burstBulletsLeft = bulletsPerBurst;
+    }
+
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+       if (currentShootingMode == ShootingMode.Auto)
         {
+            // Holding down left mouse button
+            isShooting = Input.GetKey(KeyCode.Mouse0);
+        }
+       else if (currentShootingMode == ShootingMode.Single ||
+            currentShootingMode == ShootingMode.Burst)
+        {
+            // Clicking left mouse button once
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+
+       if (readyToShoot && isShooting)
+        {
+            burstBulletsLeft = bulletsPerBurst;
             FireWeapon();
         }
     }
 
     private void FireWeapon()
     {
+        readyToShoot = false;
+
+        Vector3 shootingDirection = CalculateDiractionAndSpread().normalized;
+
         // Instantiate the bullet
         GameObject bullet = Instantiate(bulletPrefeb, bulletSpawn.position, Quaternion.identity);
+
+        // Pointing the bullet to face the shooting direction
+        bullet.transform.forward = shootingDirection;
+
         // Shoot the bullet
         bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+
         // Destroy the bullet after some time
         StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefebLifeTime));
 
+        // Checking if we are done shooting
+        if (allowReset)
+        {
+            Invoke("ResetShot", shootingDelay);
+            allowReset = false;
+        }
 
+        // Burst Mode
+        if (currentShootingMode == ShootingMode.Burst && burstBulletsLeft > 1) // we already shoot once before this check
+        {
+            burstBulletsLeft--;
+            Invoke("FireWeapon", shootingDelay);
+        }
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowReset = true;
+    }
+
+    public Vector3 CalculateDiractionAndSpread()
+    {
+        // Shooting from the middle of the screen to check where are we pointing at
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Hitting something
+            targetPoint = hit.point;
+        }
+        else
+        {
+            // Shooting at the air
+            targetPoint = ray.GetPoint(100);
+        }
+
+        Vector3 direction = targetPoint - bulletSpawn.position;
+
+        float x = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+        float y = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+
+        // Returning the shooting direction and spread
+        return direction + new Vector3(x, y, 0);
     }
 
     private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
